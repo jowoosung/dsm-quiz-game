3f3a3b1abfc2e54c203a28b2653afdc547face12
wooxist <75748267+jowoosung@users.noreply.github.com>
2025-04-08 22:52:41 +0900

    gogogo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <h1>제1회 DSM정책기획팀 퀴즈 퀴즈</h1>
-      <input placeholder="닉네임을 입력하세요." style={{ marginRight: "8px" }} />
-      <button>입장</button>
-    </main>
-  );
+  return <h1>DSM 정책기획팀 퀴즈 퀴즈</h1>;
 }
