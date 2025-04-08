a1a47feadde72186795c57b5b3c0c2ff67227024
wooxist <75748267+jowoosung@users.noreply.github.com>
2025-04-08 22:49:08 +0900

    final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,9 @@
-export default function Home() { return (<div>입장화면</div>); }
+export default function Home() {
+  return (
+    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+      <h1>제1회 DSM정책기획팀 퀴즈 퀴즈</h1>
+      <input placeholder="닉네임을 입력하세요." style={{ marginRight: "8px" }} />
+      <button>입장</button>
+    </main>
+  );
+}
